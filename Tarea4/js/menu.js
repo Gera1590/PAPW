f72572e28b2b2ba57e19bb0afea06ba5e23c3c21
Gerardo A. Guerrero flores <gerardo_1590@hotmail.com>
2018-08-20 22:00:05 -0500

    yolo >:L

---
+++ Tarea4/js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('btn-menu');
+var navi = document.getElementById('navi');
+
+btnMenu.addEventListener('click', function () {
+    navi.classList.toggle('mostrar');
+});
